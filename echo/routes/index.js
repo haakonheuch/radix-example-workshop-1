e589737b74b582100ea51dc5c525bc6855322d2d
Håkon Johan Thallaug Heuch <haakonheuch@gmail.com>
2019-03-08 12:05:36 +0100

    Update index.js

--- echo/routes/index.js
+++ echo/routes/index.js
@@ -17,7 +17,7 @@ router.get('/', function(req, res) {
     };
 
     res.set('Content-Type', 'application/json');
-    console.log('Sending response ', responseObject);
+    console.log('Sending response (hheu)', responseObject);
     res.status(200).send(responseObject);
 });
 
